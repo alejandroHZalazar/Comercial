ea35ace15fbf120cd693056ba78d6d8bbd72e97e
Alejandro Zalazar <ale_vrn_2@hotmail.com>
2023-02-18 19:02:55 -0300

    commit inicial

---
+++ Comercial/Comercial/Clases/classDatos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comercial.Clases
+{
+    class classDatos
+    {
+        MySqlConnection miCOnexion = new MySqlConnection("Server = 93.188.164.127; Database = pañalera; Uid = remoto; Password = 0315061");
+        
+        public MySqlConnection abrirConexion()
+        {
+            try
+            {
+                miCOnexion.Open();
+                return miCOnexion;
+            }
+            catch (Exception)
+            {
+                return miCOnexion;
+            }
+
+         }
+
+        public void cerrarConexion()
+        {
+            miCOnexion.Close();
+        }
+    }
+}
